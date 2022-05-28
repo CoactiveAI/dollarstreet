0e6c5a68705023ba7bf28ede1637ea78dd8965a9
Will Gaviria Rojas <will@coactive.ai>
2022-05-28 01:45:05 -0700

    Add top k results for validation and training

--- dollarstreet/run.py
+++ dollarstreet/run.py
@@ -4,6 +4,8 @@ import copy
 import torch
 import torch.backends.cudnn as cudnn
 
+from dollarstreet.utils import AverageMeter
+
 
 if torch.cuda.is_available():
     device = torch.device('cuda:0')
@@ -12,81 +14,93 @@ else:
     device = torch.device('cpu')
 
 
-def _run_epoch(model, optimizer, criterion, dataloaders, phase):
-    dataloader = dataloaders[phase]
+def _accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
 
-    if phase == 'train':
-        model.train()
-    else:
-        model.eval()
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
 
-    running_loss = 0.0
-    running_corrects = 0
-
-    # Iterate over data
-    for inputs, labels in dataloader:
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-
-        optimizer.zero_grad()
-        with torch.set_grad_enabled(phase == 'train'):
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            _, preds = torch.max(outputs, 1)
-
-            if phase == 'train':
-                loss.backward()
-                optimizer.step()
-
-        # Save batch stats
-        running_loss += loss.item() * inputs.size(0)
-        running_corrects += torch.sum(preds == labels.data)
-
-    epoch_loss = running_loss / len(dataloader.dataset)
-    epoch_acc = running_corrects.double() / len(dataloader.dataset)
-
-    print(f'({phase}) Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
-
-    return model, epoch_acc
+    res = []
+    for k in topk:
+        correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
 
 
 def _run_epochs(model, dataloaders, criterion, optimizer, num_epochs, train):
     phases = ['train', 'val'] if train else ['val']
-    since = time.time()
-    val_acc_history = []
 
+    top1_history = []
+    top5_history = []
     best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
+    best_top1 = 0.0
 
     # Iterate over epochs
+    since = time.time()
     for epoch in range(num_epochs):
         print(f'Epoch {epoch + 1}/{num_epochs}')
         print('-' * 10)
 
+        # Initialize counters
+        losses = AverageMeter()
+        top1 = AverageMeter()
+        top5 = AverageMeter()
+
         # Iterate through training and validation phase
         for phase in phases:
-            model, epoch_acc = _run_epoch(
-                model, optimizer, criterion, dataloaders, phase)
+            dataloader = dataloaders[phase]
+
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            # Iterate over data
+            for inputs, labels in dataloader:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+
+                    prec1, prec5 = _accuracy(outputs.data, labels, topk=(1, 5))
+                    losses.update(loss.data.item(), inputs.size(0))
+                    top1.update(prec1[0], inputs.size(0))
+                    top5.update(prec5[0], inputs.size(0))
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+            print(
+                f'({phase}) Loss: {losses.avg:.4f} prec@1: {top1.avg:.4f} prec@5: {top5.avg:.4f}')
 
             # Save model if it has better performance
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
+            if phase == 'val' and top1.avg > best_top1:
+                best_top1 = top1.avg
                 best_model_wts = copy.deepcopy(model.state_dict())
 
             # Save epoch stats
             if phase == 'val':
-                val_acc_history.append(epoch_acc)
+                top1_history.append(top1.avg)
+                top5_history.append(top5.avg)
 
         print()
 
     time_elapsed = time.time() - since
-    print('Complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
+    print(
+        f'Complete in {(time_elapsed // 60):.0f}m {(time_elapsed % 60):.0f}s')
+    print(f'Best prec@1: {best_top1:4f}')
 
     # load best model weights
     model.load_state_dict(best_model_wts)
-    return model, val_acc_history
+
+    return model, top1_history, top5_history
 
 
 def train_model(model, dataloaders, criterion, optimizer, num_epochs):
--- dollarstreet/utils.py
+++ dollarstreet/utils.py
@@ -1,6 +1,28 @@
 from PIL import Image
 
 
+class AverageMeter(object):
+    """Computes and stores the average and current value
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.hist = []
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.hist.append(val)
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
 def pil_loader(path: str) -> Image.Image:
     img = Image.open(path)
     return img.convert("RGB")
